774eeb9487f08725b9e25affdf3091dc1ee87d91
Bonu <bonu2530@gmail.com>
2025-12-31 14:36:01 +0900

    feat: MongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,65 @@
+console.log("Web Serverni boshlash");
+const express = require('express');
+const app = express();
+
+
+const fs = require('fs');   // Fayl tizimi moduli, fayllar bilan ishlash uchun
+
+let user;  
+fs.readFile("database/user.json", "utf-8", (err, data) => {  // user.json faylini o'qib olamiz
+  if(err){
+    console.log("Error:", err);
+} else {
+    user = JSON.parse(data);  // O'qilgan ma'lumotni JSON formatiga o'tkazamiz
+}
+});
+
+
+// MongoDB chaqirish
+const db = require('./server').db(); // server.js faylidan MongoDB mijozini chaqiramizß
+
+
+
+// 1 : Kirish code 
+app.use(express.static('public')); // public folderni statik fayllar uchun ishlatamiz
+app.use(express.json()); // JSON formatdagi so'rovlarni qabul qilish uchun
+app.use(express.urlencoded({ extended: true })); // URL-encoded so'rovlarni qabul qilish uchun, 
+                                                // html dan keladigan malumotlarni qabul qilish uchun
+
+
+
+
+// 2 : Session code
+
+
+// 3 : Views code
+app.set("views", "views"); // views papkasini shablonlar uchun sozlaymiz
+app.set("view engine", "ejs"); // EJS shablon dvigatelini sozlaymiz, BSSR(Backend Server Side Rendering) uchun
+
+
+
+// 4 : Routing code
+// app.get("/hello", function(req, res){    // root ga so'rov kelganda
+//   res.end(`<h1 style="background: red">Hello World by Bonu</h1>`);  // javob qaytaramiz
+// });
+
+// app.get("/gift", function(req, res){    // /gift ga so'rov kelganda
+//   res.end(`<h1>Siz sovgalar sahifasidasiz!</h1>`);  
+// });
+
+app.post("/create-item", (req, res) => {
+  console.log(req.body);  // kelgan malumotni konsolga chiqaramiz
+  res.json({ status: "Muvaffaqiyatli yaratildi" }); // JSON formatda javob qaytaramiz
+});
+
+app.get('/author', (req, res) => {
+  res.render("author",{user:user}); // views papkasidagi author.ejs shablonini render qilamiz
+});
+
+app.get("/", function(req, res){    // root ga so'rov kelganda
+  res.render("reja");  // views papkasidagi reja.ejs shablonini render qilamiz
+});
+
+module.exports = app; // app ni tashqi fayllarda ishlatish uchun eksport qilamiz
+
+
--- server.js
+++ server.js
@@ -1,68 +1,38 @@
-console.log("Web Serverni boshlash");
-const express = require('express');
-const app = express();
 const http = require('http');  // Node.js ning http modulini chaqiramiz
 
-const fs = require('fs');   // Fayl tizimi moduli, fayllar bilan ishlash uchun
+const mongodb = require('mongodb'); // MongoDB moduli
 
-let user;  
-fs.readFile("database/user.json", "utf-8", (err, data) => {  // user.json faylini o'qib olamiz
-  if(err){
-    console.log("Error:", err);
-} else {
-    user = JSON.parse(data);  // O'qilgan ma'lumotni JSON formatiga o'tkazamiz
-}
+let db;
+const connectionString = 'mongodb+srv://BB_db_user:hNHw0s1c3j0er4yD@cluster0.40egljy.mongodb.net/Reja?appName=Cluster0'; // MongoDB ulanish satri
+
+mongodb.connect(
+  connectionString, 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}, 
+(err, client) => {
+  if (err) 
+    console.log("MongoDB ga ulanishda xatolik yuz berdi:", err);
+  else {
+    console.log("MongoDB ga muvaffaqiyatli ulandi!");
+    module.exports = client; // MongoDB mijozini eksport qilamiz
+
+    const app = require('./app');  // app.js faylini chaqiramiz
+    const server = http.createServer(app);   // http serverini yaratamiz
+    let PORT = 3000;
+    server.listen(PORT, function(){           // serverni belgilangan portda ishga tushiramiz
+      console.log(
+        `Server ushbu manzilda ishga tushdi: http://localhost:${PORT}, http://locahost:${PORT}`
+      );
+    });
+  }
 });
 
 
-
-
-
-// 1 : Kirish code 
-app.use(express.static('public')); // public folderni statik fayllar uchun ishlatamiz
-app.use(express.json()); // JSON formatdagi so'rovlarni qabul qilish uchun
-app.use(express.urlencoded({ extended: true })); // URL-encoded so'rovlarni qabul qilish uchun, 
-                                                // html dan keladigan malumotlarni qabul qilish uchun
-
-
-
-
-// 2 : Session code
-
-
-// 3 : Views code
-app.set("views", "views"); // views papkasini shablonlar uchun sozlaymiz
-app.set("view engine", "ejs"); // EJS shablon dvigatelini sozlaymiz, BSSR(Backend Server Side Rendering) uchun
-
-
-
-// 4 : Routing code
-// app.get("/hello", function(req, res){    // root ga so'rov kelganda
-//   res.end(`<h1 style="background: red">Hello World by Bonu</h1>`);  // javob qaytaramiz
+// const server = http.createServer(app);   // http serverini yaratamiz
+// let PORT = 3000;
+// server.listen(PORT, function(){           // serverni belgilangan portda ishga tushiramiz
+//   console.log(`Server ushbu manzilda ishga tushdi: http://localhost:${PORT}, http://locahost:${PORT}`);
 // });
-
-// app.get("/gift", function(req, res){    // /gift ga so'rov kelganda
-//   res.end(`<h1>Siz sovgalar sahifasidasiz!</h1>`);  
-// });
-
-app.post("/create-item", (req, res) => {
-  console.log(req.body);  // kelgan malumotni konsolga chiqaramiz
-  res.json({ status: "Muvaffaqiyatli yaratildi" }); // JSON formatda javob qaytaramiz
-});
-
-app.get('/author', (req, res) => {
-  res.render("author",{user:user}); // views papkasidagi author.ejs shablonini render qilamiz
-});
-
-app.get("/", function(req, res){    // root ga so'rov kelganda
-  res.render("reja");  // views papkasidagi reja.ejs shablonini render qilamiz
-});
-
-
-
-const server = http.createServer(app);   // http serverini yaratamiz
-let PORT = 3000;
-server.listen(PORT, function(){           // serverni belgilangan portda ishga tushiramiz
-  console.log(`Server ushbu manzilda ishga tushdi: http://localhost:${PORT}, http://locahost:${PORT}`);
-});
 
